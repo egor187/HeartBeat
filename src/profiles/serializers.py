f4b293e3c80223d87daa485d9c7d1fe821b0fb13
egor187 <py.egor.py@gmail.com>
2021-06-30 08:36:05 +0300

    create "profile" serializer

---
+++ src/profiles/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from src.profiles.models import CustomUser
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ["id", "username", "email"]

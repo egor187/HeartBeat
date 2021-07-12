45d4ceafa37d869f00b7c3721cea9d40840fb96e
egor187 <py.egor.py@gmail.com>
2021-07-12 18:14:35 +0300

    create models for "heartbeat" app

--- src/heartbeat/models.py
+++ src/heartbeat/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+from src.profiles.models import CustomUser
+
+
+class Team(models.Model):
+    """
+    Class to define Team
+    """
+    name = models.CharField(max_length=50, verbose_name="Name of the team")
+    team_lead = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        verbose_name="team leader",
+        related_name="team_lead_teams"
+    )
+    members = models.ManyToManyField(
+        CustomUser,
+        through="Membership",
+        verbose_name="team members",
+        related_name="member_teams"
+    )
+
+    def __str__(self):
+        return f"Team: {self.name}. Team_lead: {self.team_lead}. Members: {self.members.all()}"
+
+
+class Membership(models.Model):
+    """
+    Class for extra data of membership to associate it with the relationship between two models
+    """
+    member = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="memberships")
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="membership")
+    date_joined = models.DateField(verbose_name="date of joining")
+
+    def __str__(self):
+        return f"{self.member} joined team {self.team} at {self.date_joined}"

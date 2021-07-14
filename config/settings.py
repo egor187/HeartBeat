88a4cbcb15d99a7d3750e8dcfa1f6b70352063b7
egor187 <py.egor.py@gmail.com>
2021-07-14 18:30:27 +0300

    split drf auth_classes between "prod" (base) and "dev" settings

--- config/settings.py
+++ config/settings.py
@@ -145,7 +145,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
--- config/settings_dev.py
+++ config/settings_dev.py
@@ -6,3 +6,15 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "127.0.0.1"
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}

d079f3aaa167e1d6779769bd8d88994d54aabd39
egor187 <py.egor.py@gmail.com>
2021-07-14 18:26:49 +0300

    refactor: add property "is_team_lead" to CustomUser (AUTH_MODEL)

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class CustomUser(AbstractUser):
-    pass
+    _is_team_lead = models.BooleanField(default=False)
+
+    @property
+    def is_team_lead(self):
+        return self._is_team_lead
+
+    @is_team_lead.setter
+    def is_team_lead(self, value: bool) -> None:
+        self._is_team_lead = value

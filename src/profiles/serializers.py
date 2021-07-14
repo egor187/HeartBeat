c11677558c6eff743a058335b7919cce907f3529
egor187 <py.egor.py@gmail.com>
2021-07-14 18:28:38 +0300

    add realation field (to "Team" instances) to CustomUser serializers

--- src/profiles/serializers.py
+++ src/profiles/serializers.py
@@ -4,6 +4,7 @@ from src.profiles.models import CustomUser
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = CustomUser
-        fields = ["id", "username", "email"]
+        fields = ["id", "username", "team_lead_teams"]

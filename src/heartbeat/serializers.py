bab3ba04144e6b8af0de4cbfa52cfd1fcd8813bc
egor187 <py.egor.py@gmail.com>
2021-07-18 13:54:05 +0300

    create Membership serializer and CustomUser serializer, implemented nested serialization to related field of "Team" instance

--- src/heartbeat/serializers.py
+++ src/heartbeat/serializers.py
@@ -1,13 +1,35 @@
 from rest_framework import serializers
 
-from .models import Team
+from .models import Team, Membership
+from ..profiles.models import CustomUser
+
+
+class MembershipSerializer(serializers.ModelSerializer):
+    date_joined = serializers.DateTimeField(read_only=True)
+    username = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all())
+    email = serializers.SlugField()
+
+    class Meta:
+        model = Membership
+        fields = ["id", "date_joined", "username", "email"]
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ["id", "username", "email"]
 
 
 class TeamSerializer(serializers.ModelSerializer):
-    team_lead = serializers.SlugRelatedField(
-        slug_field="username",
-        read_only=True
-    )  # slugs "username" field instead "id"
+    # slugs "username" field instead "id"
+    # team_lead = serializers.SlugRelatedField(
+    #     slug_field="username",
+    #     read_only=True
+    # )
+
+    #  Nested serializer realization
+    team_lead = CustomUserSerializer(read_only=True)
+    members = MembershipSerializer(many=True, read_only=True)
 
     class Meta:
         model = Team

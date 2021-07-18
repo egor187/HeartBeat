cd510f911db410431358a54b91222431abe3ece8
egor187 <py.egor.py@gmail.com>
2021-07-18 13:55:01 +0300

    plug "team" and "membership" endpoints to URLConf

--- src/heartbeat/urls.py
+++ src/heartbeat/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path("teams/", views.TeamListAPIView.as_view(), name="teams"),
     path("team/create/", views.TeamCreateAPIView.as_view(), name="create_team"),
     path("team/delete/<int:pk>/", views.TeamDeleteAPIView.as_view(), name="delete_team"),
+    path("team/update/<int:pk>/", views.TeamUpdateAPIView.as_view(), name="update_team"),
+
+    path("membership/", views.MembershipListAPIView.as_view(), name="memberships"),
+    path("membership/create/", views.MembershipCreateAPIView.as_view(), name="create_membership"),
+    path("membership/delete/<int:pk>/", views.MembershipDeleteAPIView.as_view(), name="delete_membership"),
 ]

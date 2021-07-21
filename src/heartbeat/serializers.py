2ddd8f1823781f19b084d88869418dd719530854
egor187 <py.egor.py@gmail.com>
2021-07-21 11:54:53 +0300

    refactor: 1. set related field "team_lead" on Question serializer with "HiddenField" and default CurrentUserDefault() for implementation auto populating self.request.user to related field of the Question instance without overriding view methods such as .perform_create, .perform_update etc. Such implementation make possible to add convenient way for "unique_together" model constraint validation on serializer level (without using custom Response with custom message about it) 2. split "Question" serializer for "read/write" purposes

--- src/heartbeat/serializers.py
+++ src/heartbeat/serializers.py
@@ -41,15 +41,31 @@ class TeamSerializer(serializers.ModelSerializer):
         fields = ["id", "name", "team_lead", "members"]
 
 
-class QuestionSerializer(serializers.ModelSerializer):
+class QuestionWriteSerializer(serializers.ModelSerializer):
+    """
+    HiddenField doesn't take a value based on user input, but from default value to make possible check for
+    uniqueness (validating need 'required' field, not 'read_only', which was needed for perform_create with
+    self.request.user) and auto populating current user instance to related field of the Question model without
+    overriding methods of ModelView/ModelViewSet (perform_create, create, update etc)
+    """
+    team_lead = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Question
         fields = ["id", "text", "team_lead"]
         read_only_fields = ["id", "team_lead"]
-        # validators = [
-        #     UniqueTogetherValidator(
-        #         queryset=Question.objects.all(),
-        #         fields=("text", "team_lead"),
-        #         message="Such question already created by current user"
-        #     )
-        # ]
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Question.objects.all(),
+                fields=("text", "team_lead"),
+                message="Such question already created by current user"
+            )
+        ]
+
+
+class QuestionReadSerializer(QuestionWriteSerializer):
+    """
+    Inherit from parent custom class to add "team_lead" id on related field for 'list' and 'retrieve' action
+    of the ViewSet
+    """
+    team_lead = None
--- src/heartbeat/views.py
+++ src/heartbeat/views.py
@@ -7,7 +7,8 @@ from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
 from .models import Team, Membership, Question
-from .serializers import TeamSerializer, MembershipReadSerializer, MembershipWriteSerializer, QuestionSerializer
+from .serializers import TeamSerializer, MembershipReadSerializer, MembershipWriteSerializer, QuestionWriteSerializer, \
+    QuestionReadSerializer
 from .custom_permissions import IsQuestionOwner, IsTeamLead
 
 
@@ -97,43 +98,52 @@ class MembershipDeleteAPIView(generics.DestroyAPIView):
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
-    serializer_class = QuestionSerializer
-    permission_classes = [IsQuestionOwner, IsTeamLead]
+    permission_classes = [IsQuestionOwner, ]
 
-    def perform_create(self, serializer):
-        serializer.save(team_lead=self.request.user)
-
-    def create(self, request, *args, **kwargs):
-        """
-        override to handle Question-model unique_together constraint
-        """
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        try:
-            self.perform_create(serializer)
-        except IntegrityError:
-            return Response(data={"message": "unique constraint violation"}, status=status.HTTP_409_CONFLICT)
+    def get_serializer_class(self):
+        if self.action in ("create", "update", "destroy"):
+            return QuestionWriteSerializer
         else:
-            headers = self.get_success_headers(serializer.data)
-            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+            return QuestionReadSerializer
 
-    def update(self, request, *args, **kwargs):
-        """
-        override to handle Question-model unique_together constraint
-        """
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
+    # unique_constrain validation and auto populating current user to related field "team-lead" provided
+    # by HiddenField and UniqueTogetherValidator on serializer class.
+    # The implementation below is less convenient and less intuitive
 
-        try:
-            self.perform_update(serializer)
-            if getattr(instance, '_prefetched_objects_cache', None):
-                # If 'prefetch_related' has been applied to a queryset, we need to
-                # forcibly invalidate the prefetch cache on the instance.
-                instance._prefetched_objects_cache = {}
-        except IntegrityError:
-            return Response(data={"message": "unique constraint violation"}, status=status.HTTP_409_CONFLICT)
-        else:
-            return Response(serializer.data)
+    # def perform_create(self, serializer):
+    #     serializer.save(team_lead=self.request.user)
+    #
+    # def create(self, request, *args, **kwargs):
+    #     """
+    #     override to handle Question-model unique_together constraint
+    #     """
+    #     serializer = self.get_serializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #
+    #     try:
+    #         self.perform_create(serializer)
+    #     except IntegrityError:
+    #         return Response(data={"message": "unique constraint violation"}, status=status.HTTP_409_CONFLICT)
+    #     else:
+    #         headers = self.get_success_headers(serializer.data)
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    #
+    # def update(self, request, *args, **kwargs):
+    #     """
+    #     override to handle Question-model unique_together constraint
+    #     """
+    #     partial = kwargs.pop('partial', False)
+    #     instance = self.get_object()
+    #     serializer = self.get_serializer(instance, data=request.data, partial=partial)
+    #     serializer.is_valid(raise_exception=True)
+    #
+    #     try:
+    #         self.perform_update(serializer)
+    #         if getattr(instance, '_prefetched_objects_cache', None):
+    #             # If 'prefetch_related' has been applied to a queryset, we need to
+    #             # forcibly invalidate the prefetch cache on the instance.
+    #             instance._prefetched_objects_cache = {}
+    #     except IntegrityError:
+    #         return Response(data={"message": "unique constraint violation"}, status=status.HTTP_409_CONFLICT)
+    #     else:
+    #         return Response(serializer.data)
